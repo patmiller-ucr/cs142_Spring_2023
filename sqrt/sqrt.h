8ccbf458401582444498776d5d15cd9e179222df
Pat Miller <122306076+patmiller-ucr@users.noreply.github.com>
2023-04-06 09:42:25 -0500

    Create sqrt.h

---
+++ sqrt/sqrt.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+namespace CS142 {
+
+  void fast_sqrt(const std::vector<double>& inputs, std::vector<double>& outputs );
+
+  void accurate_sqrt(const std::vector<double>& inputs, std::vector<double>& outputs );
+
+}
