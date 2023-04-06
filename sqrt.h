d72b92a2ef8e8c82bdab20890b639db65b2266c5
Patrick Miller <pmiller@ucr.edu>
2023-04-06 08:41:18 -0700

    Delete sqrt.h

--- sqrt.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-namespace CS142 {
-
-  void fast_sqrt(const std::vector<double>& inputs, std::vector<double>& outputs );
-
-  void accurate_sqrt(const std::vector<double>& inputs, std::vector<double>& outputs );
-
-}
